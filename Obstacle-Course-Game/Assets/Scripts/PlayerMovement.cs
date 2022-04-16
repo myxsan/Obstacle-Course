82e9b7d7add4d27a95fb6eb6f9127a52524aaf49
myxsan <yksl3461@gmail.com>
2022-04-16 15:33:04 +0300

    Configured player movement script

--- Obstacle-Course-Game/Assets/Scripts/PlayerMovement.cs
+++ Obstacle-Course-Game/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    [SerializeField] float playerSpeed = 0.0001f;
+    Transform playerTransform;
+    private void Awake() {
+        playerTransform = GetComponent<Transform>();
+    }
     void Start()
     {
         
@@ -11,6 +17,27 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        MovePlayer(playerSpeed);
+    }
+
+    private void MovePlayer(float speed)
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(-speed * Time.deltaTime,0f,0f);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(speed * Time.deltaTime,0f,0f);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(0f,0f,-speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(0f,0f,speed * Time.deltaTime);
+        }
         
     }
 }
