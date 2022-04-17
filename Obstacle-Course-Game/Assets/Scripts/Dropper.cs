975fafcb23d8179aeef4c790957e379a8e266837
myxsan <yksl3461@gmail.com>
2022-04-18 00:55:38 +0300

    There are some obstacles drops few seconds later then game starts

---
+++ Obstacle-Course-Game/Assets/Scripts/Dropper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log(Time.time);
+        if (Time.time >= 3)
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
