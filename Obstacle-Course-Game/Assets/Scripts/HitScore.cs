daf9576484bffe1c6d0ea8145acb8785bbb8ca27
myxsan <yksl3461@gmail.com>
2022-04-18 00:54:46 +0300

    There is a counter of bumping in the console

---
+++ Obstacle-Course-Game/Assets/Scripts/HitScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScore : MonoBehaviour
+{
+    public int hitTimes = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        hitTimes ++;
+        Debug.Log("You've bumped into an obstacle " + hitTimes + "X times.");
+        
+    }
+
+}
