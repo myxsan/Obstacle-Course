804409fa7c67c99b9cbb5e2de15428946485ea47
myxsan <yksl3461@gmail.com>
2022-04-17 19:30:11 +0300

    Edited PlayerMovement.cs and set up the cinemachine

--- Obstacle-Course-Game/Assets/Scripts/PlayerMovement.cs
+++ Obstacle-Course-Game/Assets/Scripts/PlayerMovement.cs
@@ -3,41 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
-    
-    [SerializeField] float playerSpeed = 0.0001f;
+{   
     Transform playerTransform;
-    private void Awake() {
-        playerTransform = GetComponent<Transform>();
-    }
+    [SerializeField] float playerSpeed;
+
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        MovePlayer(playerSpeed);
-    }
-
-    private void MovePlayer(float speed)
-    {
-        if(Input.GetKey(KeyCode.W))
-        {
-            this.transform.Translate(-speed * Time.deltaTime,0f,0f);
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            this.transform.Translate(speed * Time.deltaTime,0f,0f);
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(0f,0f,-speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            this.transform.Translate(0f,0f,speed * Time.deltaTime);
-        }
-        
+        float playerXSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        float playerZSpeed = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+        this.transform.Translate(playerXSpeed, 0.0f, playerZSpeed);
     }
 }
