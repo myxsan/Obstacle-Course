5bf9585829909941b96c239d7bc7750da5c23c3d
myxsan <yksl3461@gmail.com>
2022-04-18 00:53:48 +0300

    Wall's colour turns red when player bumps them

---
+++ Obstacle-Course-Game/Assets/Scripts/HitObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
