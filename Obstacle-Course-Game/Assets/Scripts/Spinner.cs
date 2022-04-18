3248e74a4b45ee9ba8ccfcfc4efe4ad372f9c475
myxsan <yksl3461@gmail.com>
2022-04-18 23:22:39 +0300

    Created an obstacle can spin

---
+++ Obstacle-Course-Game/Assets/Scripts/Spinner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float spinnerSpeed;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        this.gameObject.transform.Rotate(0.0f, -spinnerSpeed, 0.0f);
+    }
+}
